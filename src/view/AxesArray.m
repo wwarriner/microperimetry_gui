4aa5d6589ecb5a256d07f72da5421c7d20d410fb
wwarriner <wwarriner@gmail.com>
2021-03-01 13:26:30 -0600

    doc

--- src/view/AxesArray.m
+++ src/view/AxesArray.m
@@ -1,9 +1,10 @@
 classdef AxesArray < handle
-    properties
-        axis_size (1,2) double = [320, 320]
-        in_padding (1,2) double = [20, 20]
-    end
-    
+    %{
+    AxesArray encapsulates a specific arrangement of axes objects in a
+    container. The container and rows by columns are passed in as arguments to
+    the constructor. Allows two colorbar objects to be used, one on either side.
+    This object should have the same parent as the two colorbar objects.
+    %}
     properties (SetAccess = private)
         left_colorbar Colorbar
         right_colorbar Colorbar
@@ -11,30 +12,52 @@ classdef AxesArray < handle
     
     methods
         function obj = AxesArray(parent, row_count, column_count)
+            %{
+            Inputs:
+            parent - container object (e.g. figure, uipanel)
+            row_count - number of rows of axes
+            column_count - number of columns of axes
+            %}
             obj.parent = parent;
             obj.row_count = row_count;
             obj.column_count = column_count;
         end
         
         function set_left_colorbar(obj, cb)
+            %{
+            Sets a Colorbar object as the left colorbar. Matched with
+            appropriate axes.
+            %}
             cb.location = "west";
             cb.update();
             obj.left_colorbar = cb;
         end
         
         function set_right_colorbar(obj, cb)
+            %{
+            Sets a Colorbar object as the right colorbar. Matched with
+            appropriate axes.
+            %}
             cb.location = "east";
             cb.update();
             obj.right_colorbar = cb;
         end
         
         function set_axes(obj, ax, row, col)
+            %{
+            Add an axes-like object to one of this object's slots at the supplied row
+            and column position.
+            %}
             assert(0 <= row && row <= obj.row_count);
             assert(0 <= row && row <= obj.row_count);
             obj.axes_handles{row, col} = ax;
         end
         
         function apply(obj, fn)
+            %{
+            Applies a function to all axes. Function must accept an axes-like
+            object (set with set_axes) as well as a row and column.
+            %}
             for row = 1 : obj.row_count
                 for col = 1 : obj.column_count
                     ax = obj.axes_handles{row, col};
@@ -44,11 +67,17 @@ classdef AxesArray < handle
         end
         
         function update_layout(obj)
-            obj.compute_padding();
+            %{
+            Updates the layout of the axes in the array.
+            %}
+            [obj.pre_pad, obj.post_pad] = obj.update_padding();
             obj.apply(@(ax,row,col)obj.update_position(ax, row, col));
         end
 
         function set_parent(obj, new_parent)
+            %{
+            Sets the parent of this object and all its children.
+            %}
             if ~isempty(obj.axes_handles)
                 obj.apply(@(ax,row,col)ax.set_parent(new_parent));
             end
@@ -74,7 +103,8 @@ classdef AxesArray < handle
     end
     
     properties (Access = private, Constant)
-        COLORBAR_PADDING (1,2) double = [70 0] % HACK, colorbar should know its size
+        AXIS_SIZE (1,2) double = [320, 320]
+        IN_PADDING (1,2) double = [20, 20]
     end
     
     methods (Access = private)
@@ -83,22 +113,26 @@ classdef AxesArray < handle
             ax.set_position(position);
         end
         
-        function compute_padding(obj)
-            full = obj.parent.Position(3:4);
+        function [pre, post] = update_padding(obj)
+            available = obj.parent.Position(3:4);
             counts = [obj.column_count, obj.row_count];
-            pad = full - (counts - 1) .* obj.in_padding - 2 .* obj.COLORBAR_PADDING;
-            pad = pad - counts .* obj.axis_size;
+            inner_pad = (counts - 1) .* obj.IN_PADDING;
+            colorbar_pad = 2 .* [Colorbar.WIDTH, 0];
+            axis_use = counts .* obj.AXIS_SIZE;
+            pad = available - inner_pad - colorbar_pad - axis_use;
             assert(all(0 < pad));
-            obj.pre_pad = round(0.5 * pad);
-            obj.post_pad = pad - obj.pre_pad;
+            pre = round(0.5 * pad);
+            post = pad - pre;
         end
         
         function pos = compute_position(obj, col, row)
-            x = obj.pre_pad(1) + (col - 1) * (obj.axis_size(1) + obj.in_padding(1)) ...
-                + obj.COLORBAR_PADDING(1);
-            y = obj.post_pad(2) + (row - 1) * (obj.axis_size(2) + obj.in_padding(2));
-            w = obj.axis_size(1);
-            h = obj.axis_size(2);
+            x = obj.pre_pad(1) ...
+                + (col - 1) * (obj.AXIS_SIZE(1) + obj.IN_PADDING(1)) ...
+                + Colorbar.WIDTH;
+            y = obj.post_pad(2) ...
+                + (row - 1) * (obj.AXIS_SIZE(2) + obj.IN_PADDING(2));
+            w = obj.AXIS_SIZE(1);
+            h = obj.AXIS_SIZE(2);
             pos = [x y w h];
         end
     end
